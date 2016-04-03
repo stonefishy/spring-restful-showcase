1f6f47a4d8a534d717fb470334c8a2d9f5424de3
Andrew <andrewsy@163.com>
2016-04-04 01:05:00 +0800

    using spring boot to host the application

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Andrewsy on 2016/4/4.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
